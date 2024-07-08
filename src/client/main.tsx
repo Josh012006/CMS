94b1a108c173882aa00224218bde1257ce3796e2
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2024-07-08 23:42:28 +0000

    Some minor improvements

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -5,7 +5,6 @@ import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
 
-
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
